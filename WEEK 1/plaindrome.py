be73b28f5e49d462ea61b7845995d9c4ab95973a
minnubenny <61495220+minnubenny@users.noreply.github.com>
2025-05-07 12:24:09 +0530

    Delete WEEK 1/plaindrome.py

--- WEEK 1/plaindrome.py
+++
@@ -1,8 +0,0 @@
-def is_palindrome(s):
-
-    cleaned = ''.join(c.lower() for c in s if c.isalnum())# Remove non-alphanumeric characters and convert to lowercase
-    return cleaned == cleaned[::-1]# Check if cleaned string is equal to its reverse
-
-# Example usage:
-print(is_palindrome("malayalam")) 
-print(is_palindrome("hello"))                 
